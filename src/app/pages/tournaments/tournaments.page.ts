d7e6aa14760ae974eb05a4a696fe821b2b09c2b1
Json Alzate <developer.alzate@gmail.com>
2025-08-06 12:01:47 -0500

    ➕  Añadir FullCalendar a la página de torneos. Se actualizan package.json para incluir las dependencias de FullCalendar y se modifica el HTML y TypeScript de la página para integrar el calendario.

--- src/app/pages/tournaments/tournaments.page.ts
+++ src/app/pages/tournaments/tournaments.page.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonAvatar, IonButton, IonItem, IonLabel, IonFabButton, IonFab, IonIcon } from '@ionic/angular/standalone';
+
+
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
 
@@ -10,12 +16,15 @@ import { add } from 'ionicons/icons';
   templateUrl: './tournaments.page.html',
   styleUrls: ['./tournaments.page.scss'],
   standalone: true,
-  imports: [IonFab, IonFabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonAvatar, IonButton, IonItem, IonLabel, IonIcon]
+  imports: [IonFab, IonFabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonAvatar, IonButton, IonItem, IonLabel, IonIcon, FullCalendarModule]
 })
 export class TournamentsPage implements OnInit {
 
   addIcon = add;
-
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
   constructor() {
     addIcons({ add });
    }
