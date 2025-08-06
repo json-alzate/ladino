001a26057d76e9a82f5f2fa054632bec916980fe
Json Alzate <developer.alzate@gmail.com>
2025-08-06 07:59:38 -0500

    Pagina de torneos

--- src/app/pages/tournaments/tournaments.page.ts
+++ src/app/pages/tournaments/tournaments.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonAvatar, IonButton, IonItem, IonLabel, IonFabButton, IonFab, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tournaments',
   templateUrl: './tournaments.page.html',
   styleUrls: ['./tournaments.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonFab, IonFabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonAvatar, IonButton, IonItem, IonLabel, IonIcon]
 })
 export class TournamentsPage implements OnInit {
 
-  constructor() { }
+  addIcon = add;
+
+  constructor() {
+    addIcons({ add });
+   }
 
   ngOnInit() {
   }
